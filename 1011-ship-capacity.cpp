c7a33bb961abeddf45e579361e2cf57f4578b1aa
Arpitpundir <thakurarpitpundir73@gmail.com>
2021-10-19 23:56:52 +0530

    solved 1011

---
+++ 1011-ship-capacity.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int check(vector<int> w, int c){
+        int d = 0;
+        int n = w.size(), currC = 0;
+        for(int i=0;i<n;i++){
+            if(currC+w[i]>c){
+                d++;
+                currC = 0;
+            }
+            currC+=w[i];
+        }
+        if(currC!=0) d+=1;
+        return d;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        long int n = weights.size();
+        long int end = accumulate(weights.begin(), weights.end(), 0L);
+        long int st = *max_element(weights.begin(), weights.end());
+        cout<<st<<endl;
+        
+        while(st<end){
+            long int mid = st+floor((end-st)/2);
+            int d = check(weights,mid);
+            //cout<<d<<endl;
+            if(d > days){
+                st = mid+1;
+            }else{
+                end = mid;
+            }
+        }
+        return st;
+    }
+};
